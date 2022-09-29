36046d6984716afd3de5fceb81b85f2a21230745
bruno <bruno.kalfa@gmail.com>
2022-09-29 15:45:21 +0200

    Importation de packages externes et ajout des fonctions Gradient selon x et y

--- codeinpainting.py
+++ codeinpainting.py
@@ -6,3 +6,25 @@ Created on Thu Sep 29 15:04:32 2022
 @author: brunokalfa
 """
 
+#%% SECTION 1 inclusion de packages externes 
+
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io as skio
+
+
+#%% SECTION 2 Fonctions utiles
+
+def gradx(im):
+    "renvoie le gradient dans la direction x"
+    imt=np.float32(im)
+    gx=0*imt
+    gx[:,:-1]=imt[:,1:]-imt[:,:-1]
+    return gx
+
+def grady(im):
+    "renvoie le gradient dans la direction y"
+    imt=np.float32(im)
+    gy=0*imt
+    gy[:-1,:]=imt[1:,:]-imt[:-1,:]
+    return gy
